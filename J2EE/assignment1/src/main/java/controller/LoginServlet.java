830ae5260617e4dd978622ca016c5bfaa51dbd01
jeven <161250109@smail.nju.edu.cn>
2018-12-23 19:39:39 +0800

    j2ee作业一web项目目录搭建好

---
+++ J2EE/assignment1/src/main/java/controller/LoginServlet.java
@@ -0,0 +1,25 @@
+package controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by jeven on 2018/12/23.
+ */
+@WebServlet(value = "/login")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("This is a new servlet page2");
+    }
+}
